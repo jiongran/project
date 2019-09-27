3d5e88b43d9f0bd7c64b914782f55d8029bbfae1
lijin <lijin2416059@163.com>
2019-09-27 16:23:49 +0800

    添加路由切换进度条
    添加manifest.json配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 import setting from '@/settings'
 import * as util from '@/util/cookies'
+
+NProgress.configure({
+  parent: '#app',
+  easing: 'ease', // 动画方式
+  speed: 500, // 递增进度条的速度
+  showSpinner: false, // 是否显示加载ico
+  trickleSpeed: 200, // 自动递增间隔
+  minimum: 0.3 // 初始化时的最小百分比
+})
 
 Vue.use(Router)
 
@@ -57,14 +68,18 @@ const router = new Router({
 })
 
 router.beforeEach((to, from, next) => {
+  NProgress.start()
   if (to.meta.requiresAuth) {
     if ((util.getToken(setting.defaultStorageState) as any)) {
       next()
+      NProgress.done()
     } else {
       next({ name: 'login' })
+      NProgress.done()
     }
   } else {
     next()
+    NProgress.done()
   }
 })
 
